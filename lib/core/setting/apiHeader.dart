01c9343031c985984b019658427243c1e7d9fbda
RahmanSadikur <rahmansadikur9989@gmail.com>
2022-02-14 13:06:54 +0600

    init

--- lib/core/setting/apiHeader.dart
+++ lib/core/setting/apiHeader.dart
@@ -2,7 +2,7 @@ class ApiHeaders {
   static const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'App-Key': '',
+    'App-Key': 'AiubPortalMobileAppBy\$DD2019',
     'Access-Control-Allow-Origin': '*',
   };
 }
