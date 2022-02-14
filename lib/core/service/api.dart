64cc4581be962075068545f4254ea7c8a9bc574d
RahmanSadikur <55327852+RahmanSadikur@users.noreply.github.com>
2022-02-14 13:08:39 +0600

    Update api.dart

--- lib/core/service/api.dart
+++ lib/core/service/api.dart
@@ -1,8 +1,8 @@
 class Api {
-  // static const baseUrl = 'https://testapi.aiub.edu/ums-public-api/api/';
-  // static const tokenUrl = 'https://testapi.aiub.edu/ums-auth-api/token';
-  static const noticeUrl = 'https://www.aiub.edu/getLatestNotices';
+  // static const baseUrl = '';
+  // static const tokenUrl = '';
+  static const noticeUrl = '';
   // Local
-  static const baseUrl = 'http://172.16.32.122:60001/UMS_API/api/';
-  static const tokenUrl = 'http://172.16.32.122:60001/UMS_AUTH/token';
+  static const baseUrl = '';
+  static const tokenUrl = '';
 }
